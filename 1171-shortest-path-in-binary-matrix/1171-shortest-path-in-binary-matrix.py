61d6c58f328ee6a02ab7173fdec8fdcd78110d3b
Bhakti Bagadia <123622039+bhaktibagadia@users.noreply.github.com>
2024-08-12 11:41:59 +0530

    Time: 363 ms (94.94%), Space: 16.7 MB (96.39%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,34 +1,21 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
+        if grid[0][0] or grid[n-1][n-1]:
             return -1
-        if n == 1:
-            return 1 if grid[0][0] == 0 else -1
+        dir = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
 
-        # Directions for moving in 8 possible ways
-        directions = [
-            (1, 0), (1, 1), (0, 1), (-1, 1),
-            (-1, 0), (-1, -1), (0, -1), (1, -1)
-        ]
-
-        # BFS setup
-        queue = deque([(0, 0, 1)])  # (i, j, path_length)
-        grid[0][0] = 1  # mark as visited
+        queue = deque([(0,0,1)])
+        grid[0][0]=1
 
         while queue:
-            i, j, path_length = queue.popleft()
-
-            if i == n - 1 and j == n - 1:
-                return path_length
-
-            for di, dj in directions:
-                ni, nj = i + di, j + dj
-                if 0 <= ni < n and 0 <= nj < n and grid[ni][nj] == 0:
-                    queue.append((ni, nj, path_length + 1))
-                    grid[ni][nj] = 1  # mark as visited
-
-        return -1
+            i, j, path = queue.popleft()
+            if i==n-1 and j==n-1:
+                return path
+            for di, dj in dir:
+                deli = di+i
+                delj = dj+j
+                if 0<=deli<n and 0<=delj<n and grid[deli][delj]==0:
+                    queue.append((deli, delj, path+1))
+                    grid[deli][delj] = 1
+        return -1            
