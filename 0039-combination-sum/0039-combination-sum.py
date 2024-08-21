a9540cb621e0ebfae56a3617c80138cee690e37d
Bhakti Bagadia <123622039+bhaktibagadia@users.noreply.github.com>
2024-08-21 10:10:33 +0530

    Time: 47 ms (82.34%), Space: 16.7 MB (40.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def rec(i,stack, sumofstack):
-            if sumofstack==target:
-                return ds.append(stack[:])
+        ans = []
+        def rec(i, ds, summ):
             if i==len(candidates):
                 return
-            if sumofstack>target:
+            if summ ==  target:
+                ans.append(ds[:])
                 return
-            nottake = rec(i+1, stack, sumofstack)
-            if sumofstack<target:
-                stack.append(candidates[i])
-                take = rec(i, stack,sumofstack+candidates[i])
-                stack.pop()
-        ds=[]
+            if summ+candidates[i]<=target:    
+                ds.append(candidates[i])
+                summ+=candidates[i]
+                rec(i, ds, summ)
+                summ-=candidates[i]
+                ds.pop()
+            rec(i+1, ds, summ)  
         rec(0, [], 0)
-        return ds 
+        return ans          
