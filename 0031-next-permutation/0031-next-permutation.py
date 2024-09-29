b5f168838aaf8cbfca3aeccf50a2add1afccefa2
Bhakti Bagadia <123622039+bhaktibagadia@users.noreply.github.com>
2024-09-29 17:11:01 +0530

    Time: 49 ms (16.09%), Space: 16.6 MB (37.34%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,17 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+
         dip = -1
         for i in range(len(nums)-2, -1, -1):
-            if nums[i]<nums[i+1]:
+            if nums[i+1]>nums[i]:
                 dip = i
                 break
         if dip == -1:
-            nums.reverse()
-        else:            
-            for i in range(len(nums)-1, -1, -1):   
-                if nums[i]>nums[dip]:
-                    nums[i], nums[dip] = nums[dip], nums[i]
-                    break
-        nums[dip+1:] = sorted(nums[dip+1:])                 
-        
+            nums.reverse()        
+        for i in range(len(nums)-1, -1, -1):
+            if nums[dip]<nums[i]:
+                nums[dip], nums[i] = nums[i], nums[dip]
+                break
+        nums[dip+1:]=sorted(nums[dip+1:])               
