f506ea283402f83ce8fd90a3832510ff47342cab
Bhakti Bagadia <123622039+bhaktibagadia@users.noreply.github.com>
2024-09-29 14:33:46 +0530

    Time: 47 ms (6.71%), Space: 16.4 MB (94.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -5,13 +5,12 @@ class Solution:
         """
         low, mid, high = 0, 0, len(nums)-1
         while mid<=high:
-            if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
-                mid+=1
+            if nums[mid]==0:
+                nums[mid], nums[low] = nums[low], nums[mid]
                 low+=1
-            elif nums[mid] == 1:
+                mid+=1
+            elif nums[mid]==1:
                 mid+=1
             else:
-                nums[mid], nums[high] = nums[high], nums[mid]
+                nums[mid], nums[high] = nums[high], nums[mid] 
                 high-=1
-        return nums                
